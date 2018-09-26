98fe83ddc11fcb1935ede0b8b6f0fd6a9c696ad4
hsulliv <hsulliv@purdue.edu>
2018-09-25 20:10:42 -0400

    Implemented Dice Roller in toolbar

--- fantasy-club/src/app/diceroller/diceroller.component.ts
+++ fantasy-club/src/app/diceroller/diceroller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { empty, EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-diceroller',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DicerollerComponent implements OnInit {
 
+  amount : number;
+  type : number;
+  mod : number;
+  rolls : Array<number>;
+  total : number;
   constructor() { }
 
   ngOnInit() {
   }
 
+  DiceRoller() {
+    this.amount = parseInt((document.getElementById("amount") as HTMLInputElement).value);
+    this.type = parseInt((document.getElementById("type") as HTMLInputElement).value);
+    this.mod = parseInt((document.getElementById("modifier") as HTMLInputElement).value);
+    if((document.getElementById("amount") as HTMLInputElement).value == "") {
+      this.amount = 0;
+    }
+    if((document.getElementById("type") as HTMLInputElement).value == "") {
+      this.type = 0;
+    }
+    if((document.getElementById("modifier") as HTMLInputElement).value == "") {
+      this.mod = 0;
+    }
+    var i = 0;
+    this.rolls = [this.amount];
+    this.total = 0;
+    for(i = 0; i < this.amount; i++) {
+      var result = Math.floor(Math.random() * this.type) + 1;
+      this.rolls[i] = result;
+      this.total = this.total + result;
+    }
+    this.total = this.total + this.mod;
+    console.log(this.amount);
+    console.log(this.type);
+    console.log(this.mod);
+    console.log(this.total);
+    
+  }
 }
