5417cc818e4998f57e211e40e6d82755cbfefa4e
PJSmagacz2897 <jsmagac@purdue.edu>
2018-10-05 13:12:58 -0400

    "build script and some test additionas

--- fantasy-club/src/app/gm/gm.component.spec.ts
+++ fantasy-club/src/app/gm/gm.component.spec.ts
@@ -6,6 +6,7 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 describe('GmComponent', () => {
   let component: GmComponent;
   let fixture: ComponentFixture<GmComponent>;
+  let sidebar: SidebarComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,9 +20,23 @@ describe('GmComponent', () => {
     fixture = TestBed.createComponent(GmComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    sidebar = TestBed.get(SidebarComponent);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should construct', () => {
+    expect(component.app).toEqual(sidebar.app);
+    expect(component.userId).toEqual(sidebar.user_id);
+  });
+
+  it('should get dice rolls', () => {
+    let oldName: string = (document.getElementById("dice-username") as HTMLInputElement).value;
+    (document.getElementById("dice-username") as HTMLInputElement).value = "";
+    component.getDiceRolls();
+    
+  });
+
 });
