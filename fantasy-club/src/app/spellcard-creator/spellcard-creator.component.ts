b05908abc6eba89134b3c531b748c0ee75ff292a
Hunter <hsulliv@purdue.edu>
2018-10-30 19:16:06 -0400

    Added spellcard generator

--- fantasy-club/src/app/spellcard-creator/spellcard-creator.component.ts
+++ fantasy-club/src/app/spellcard-creator/spellcard-creator.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-spellcard-creator',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpellcardCreatorComponent implements OnInit {
 
-  constructor() { }
+  @Input() userId;
+  @Input() app;
+  @Output() refresh = new EventEmitter<string>();
+  name: string;
+  desc: string;
+  public: string;
+  snapshot: firebase.database.DataSnapshot;
+
+  constructor(private sidebar: SidebarComponent) {
+    this.userId = sidebar.user_id
+    this.app = firebase.app;
+  }
 
   ngOnInit() {
+  }
+
+  isUserGM() {
+    if (this.sidebar.user_priv >= 2) {
+      return true;
+    }
+    return false;
+  }
+
+  isUserLogged() {
+    if (this.sidebar.user_id != "") {
+      return true;
+    }
+    return false;
+  }
+
+  createPrivateSpell() {
+    this.name = ((document.getElementById("spellName") as HTMLInputElement).value);
+    this.desc = ((document.getElementById("spellDesc") as HTMLInputElement).value);
+    if (this.name == "" || this.desc == "") {
+      return;
+    }
+    firebase.database().ref('spellcards/private/' + this.sidebar.user_id + "/" + this.name).set(
+      {
+        creatorID: firebase.auth().currentUser.uid,
+        creatorName: firebase.auth().currentUser.displayName,
+        desc: this.desc
+      }
+    )
+  }
+
+  createPublicSpell() {
+    this.name = ((document.getElementById("spellName") as HTMLInputElement).value);
+    this.desc = ((document.getElementById("spellDesc") as HTMLInputElement).value);
+    firebase.database().ref('spellcards/public/' + this.name + "/").set(
+      {
+        creatorName: firebase.auth().currentUser.displayName,
+        creatorID: firebase.auth().currentUser.uid,
+        desc: this.desc
+      }
+    )
   }
 
 }
