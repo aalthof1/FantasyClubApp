383ac58717bd95e34991416e47d6a35c12293237
Brandon Loi <brandon.loi@gmail.com>
2018-10-30 15:30:05 -0400

    hide search if not admin

--- fantasy-club/src/app/char-viewer/char-viewer.component.ts
+++ fantasy-club/src/app/char-viewer/char-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-char-viewer',
@@ -6,13 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./char-viewer.component.css']
 })
 export class CharViewerComponent implements OnInit {
-  displayToggle : boolean = false;
+  displayToggle: boolean = false;
+  sharedToggle: boolean = false;
+  GMStatus: boolean = false;
+  adminStatus : boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   charViewerToggle() {
+    if (firebase.auth().currentUser == null) {
+      this.GMStatus = false;
+      this.adminStatus = false;
+      return;
+    }
     this.displayToggle = !this.displayToggle;
+    this.isUserGM();
+  }
+  isUserGM(): boolean {
+    if (firebase.auth().currentUser == null) {
+      this.GMStatus = false;
+      return false;
+    }
+    firebase.database().ref("user_id/" + firebase.auth().currentUser.uid + "/priv").once("value")
+      .then(function (snapshot) {
+        if (snapshot.val() >= 2) {
+          this.GMStatus = true;
+          if (snapshot.val() == 3) {
+            this.adminStatus = true;
+          }
+          return true;
+        }
+        else {
+          this.GMStatus = false;
+          this.adminStatus = false;
+          return false;
+        }
+      }.bind(this))
   }
 }
